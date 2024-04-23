6827bec3c34bdbb22c472557c93ec53ff6ae25a9
Teja7789 <rx100789@gmail.com>
2024-04-23 14:48:17 +0530

    JWT*

--- foodApp/Practice/Routers/authHelper.js
+++ foodApp/Practice/Routers/authHelper.js
@@ -1,8 +1,14 @@
 // let flag = true;
-
+const  jwt = require('jsonwebtoken');
+const JWT_KEY = "secretkey";
 function protectedRoute(req,res,next){
-    if(req.cookies.isLoggedIn){
+    if(req.cookies.login){
+        console.log(req.cookies.login,"JWT")
+        let isVerified = jwt.verify(req.cookies.login,JWT_KEY);
+      if(isVerified){
        next();
+      }else{ res.send("user not  verified");
+    } 
     }else{
         return res.json({
             message:'operation not allowed'
